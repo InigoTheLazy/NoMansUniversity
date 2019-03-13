f24ff9f6afced7c92651df1b10b191a6c360cb7f
Aurelijus <aurelijus.var@gmail.com>
2019-03-13 07:56:13 +0200

    Pradėta battle systema
    Paspaudus mygtuką, herojus puola, o po jo puola priešas.

---
+++ No Man's University/Assets/Battle/enemycon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemycon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (battleflow.whichturn == 2)
+        {
+            GetComponent<Animator>().SetTrigger("enemyAttack");
+            battleflow.whichturn = 1;
+        }
+    }
+
+   
+}
---
+++ No Man's University/Assets/Battle/herocon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class herocon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z") && battleflow.whichturn == 1)
+        {
+            GetComponent<Animator>().SetTrigger("heroAttack");
+            StartCoroutine(turnEnd());
+        }
+    }
+
+    IEnumerator turnEnd()
+    {
+        yield return new WaitForSeconds(4);
+        battleflow.whichturn = 2;
+    }
+}
+
