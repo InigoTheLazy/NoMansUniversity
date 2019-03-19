700d6b53e7da6808afae61b1676986e2adabd3e0
Dinaras Klykūnas <dinaras.klykunas@ktu.edu>
2019-03-19 16:26:23 +0200

    Patobulintas meniu

---
+++ No Man's University/Assets/Scripts/ButtonFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void HoverSound()
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+    public void ClickSound()
+    {
+        audioSource.PlayOneShot(clickSound);
+    }
+}
