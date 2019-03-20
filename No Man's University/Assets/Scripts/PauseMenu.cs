8ae4b5ad3808255ddeea9acac6d92902baca2497
Dinaras Klykūnas <dinaras.klykunas@ktu.edu>
2019-03-20 15:36:12 +0200

    Pridėtas "pause menu"

---
+++ No Man's University/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        SceneManager.LoadScene("WalkingScene");
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        SceneManager.LoadSceneAsync("PauseMenuScene");
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
