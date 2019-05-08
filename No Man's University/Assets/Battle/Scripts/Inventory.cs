d22363708bc4f5cd0f90983b3625b5dbb26be1bc
Aurelijus <aurelijus.var@gmail.com>
2019-05-08 16:25:55 +0300

    Inventory update
    Invetory, item pick up

--- No Man's University/Assets/Battle/Scripts/Inventory.cs
+++ No Man's University/Assets/Battle/Scripts/Inventory.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     private bool inventoryEnabled;
-
     public GameObject InventoryPanel;
 
     private int allSlots;
@@ -13,6 +12,7 @@ public class Inventory : MonoBehaviour
     private GameObject[] slot;
 
     public GameObject slotHolder;
+
     void Start()
     {
         inventoryEnabled = true;
@@ -22,6 +22,9 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < allSlots; i++)
         {
             slot[i] = slotHolder.transform.GetChild(i).gameObject;
+                
+            if (slot[i].GetComponent<Slot>().item = null)
+                slot[i].GetComponent<Slot>().empty = true;
         }
     }
 
@@ -38,6 +41,44 @@ public class Inventory : MonoBehaviour
         if (!inventoryEnabled)
         {
             InventoryPanel.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+            Item item = itemPickedUp.GetComponent<Item>();
+
+            AddItem(itemPickedUp, item.ID, item.type, item.description, item.icon);
+        }
+    }
+
+    private void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Sprite itemIcon)
+    {
+        for (int i = 0; i < allSlots; i++)
+        {
+            if (slot[i].GetComponent<Slot>().empty)
+            {
+                // add item to slot
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().icon = itemIcon;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().ID = itemID;
+                slot[i].GetComponent<Slot>().description = itemDescription;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+                slot[i].GetComponent<Slot>().UpdateSlot();
+                slot[i].GetComponent<Slot>().empty = false;
+
+            }
+
+            return;
         }
     }
 }
