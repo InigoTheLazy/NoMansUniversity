6a201be277cc320dcef3c2430d0ef0b3377c55b3
Ignas <inigothelazy@gmail.com>
2019-06-20 18:00:34 +0300

    Currency Part 1

---
+++ No Man's University/Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public int PMoney;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddMoney(int amount)
+    {
+        PMoney += amount;
+    }
+    public void SubtractMoney(int amount)
+    {
+        if (PMoney - amount < 0)
+        {
+            Debug.Log("Not enough money");
+        }
+        else
+        {
+            PMoney -= amount;
+        }
+    }
+}
