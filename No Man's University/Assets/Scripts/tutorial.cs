3baf5f65ae69e3ac82c56d8c67ec166cdf2782be
PijusBB <48226270+PijusBB@users.noreply.github.com>
2019-06-21 03:57:10 +0300

    fix v4

---
+++ No Man's University/Assets/Scripts/tutorial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tutorial : MonoBehaviour
+{
+    public string dialogue;
+    private DialogueManager dMAn;
+    private bool playerinzone;
+    void Start()
+    {
+        playerinzone = false;
+        dMAn = FindObjectOfType<DialogueManager>();
+    }
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerinzone = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerinzone = false;
+            if (dMAn != null)
+                dMAn.DisableBox();
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            dMAn.ShowBox(dialogue);
+        }
+
+    }
+}
